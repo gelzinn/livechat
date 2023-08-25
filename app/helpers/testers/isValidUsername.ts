ee343559a2a4518c608a0f9c3db1e56fb23354d6
gelzinn <60621494+gelzinn@users.noreply.github.com>
2023-08-25 14:26:53 -0300

    fix(helpers/testers): add exception to prevent usernames has '@'

--- app/helpers/testers/isValidUsername.ts
+++ app/helpers/testers/isValidUsername.ts
@@ -1,4 +1,5 @@
 export const isValidUsername = (username: string) => (
     /^[a-z._]+$/i.test(username) &&
-    !/^\.|_|[._]{2}|[._]$|\.(_|\.)/.test(username)
+    !/^\.|_|[._]{2}|[._]$|\.(_|\.)/.test(username) &&
+    !/@/.test(username)
 );
