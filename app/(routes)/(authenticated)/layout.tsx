276296f7d698d9da67fa9a91a825d1db5459a743
gelzinn <mcnoobend@gmail.com>
2023-08-30 17:23:41 -0300

    feat: add authenticated layout to prevent specific actions

---
+++ app/(routes)/(authenticated)/layout.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { useEffect } from "react";
+
+import { useAuth } from "app/hooks/useAuth";
+import { useRouter } from "next/navigation";
+
+export default function AuthenticatedLayout({ children }: any) {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) router.push("/");
+  }, [user]);
+
+  return children;
+}
