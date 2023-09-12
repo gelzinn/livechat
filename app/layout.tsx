ecd98c3cb6c739fc9fc38491a52975cdbca0e061
gelzinn <60621494+gelzinn@users.noreply.github.com>
2023-09-12 14:16:13 -0300

    feat: initial seo metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,21 @@ export default function RootLayout({ children }: any) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link rel="shortcut icon" href="../favicon.png" type="image/x-icon" />
+
+        <meta name="theme-color" content="#080808" />
+        <meta name="description" content="Connect and converse with real-time chat" />
+        <meta name="keywords" content="chat, live chat, chat online, chat app, chat application" />
+
+        <meta property="og:title" content="Live Chat - Connect and converse with real-time chat" />
+        <meta property="og:description" content="Connect and converse with real-time chat" />
+        <meta property="og:image" content="https://timechat.vercel.app/og-image.png" />
+        <meta property="og:url" content="https://timechat.vercel.app" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:title" content="Live Chat - Connect and converse with real-time chat" />
+        <meta name="twitter:description" content="Connect and converse with real-time chat" />
+        <meta name="twitter:image" content="https://timechat.vercel.app/og-image.png" />
+        <meta name="twitter:card" content="summary_large_image" />
       </head>
 
       <UserAgentContextProvider>
